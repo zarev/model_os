9691028f8a27b50b9c01f2e40536e22b63e82280
Mariyan <mzarev1@gmail.com>
2024-08-19 16:21:56 +0200

    moving from onnx to tf for serving

--- model_os.py
+++ model_os.py
@@ -1,7 +1,6 @@
 """model_os module"""
 
 # uvicorn model_os:app --reload
-# pylint: disable=no-member
 # pylint: disable=fixme
 
 import queue
@@ -10,7 +9,8 @@ from contextlib import asynccontextmanager
 from typing import Optional
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import onnxruntime_genai as og
+from PIL import Image
+from transformers import AutoModelForCausalLM, AutoProcessor
 
 
 @asynccontextmanager
@@ -70,10 +70,17 @@ class Model:
 
     def load(self):
         """Loads model components"""
-        # ONNX specific model loading logic
-        self.model = og.Model(self.model_path)  # pylint: disable=no-member
-        self.processor = self.model.create_multimodal_processor()
-        self.tokenizer_stream = self.processor.create_stream()
+        # tensorflow model loading logic
+        model_path = "microsoft/Phi-3-vision-128k-instruct"
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            device_map="cuda",
+            trust_remote_code=True,
+            torch_dtype="auto",
+            # Use _attn_implementation='eager' to disable flash attention, or 'flash_attention_2'
+            _attn_implementation="eager")
+
+        self.processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
         self.loaded = True
 
     def run(self, input_data: tuple[Optional[str], str], output: queue.Queue) -> None:
@@ -97,18 +104,27 @@ class Model:
         """
         # TODO add file input option
         input_image, input_text = input_data
-        template_prompt = "<|user|>\n"
+
+        messages = [{
+            "role": "user",
+            "content": ''}]
 
         image_obj = None
         # If there is image load it and add tag to prompt
         if input_image:
-            image_obj = og.Images.open(input_image)  # pylint: disable=no-member
-            template_prompt += "<|image_1|>\n"
+            image_obj = [Image.open(input_image)]  # pylint: disable=no-member
+            messages[0]["content"] += "<|image_1|>"
 
-        template_prompt += f"{input_text}\n<|end|>\n<|assistant|>"
+        messages[0]["content"] += f"{input_text}"
+
+        templated_prompt = self.processor.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True)
 
         try:
-            inputs = self.processor(template_prompt, images=image_obj)
+            inputs = self.processor(templated_prompt, image_obj, return_tensors="pt").to("cuda:0")
+
         except RuntimeError as e:
             # for some reason the error only goes to output
             # if there is a print statement first, otherwise
@@ -118,20 +134,23 @@ class Model:
             output.put(f"Error occurred during input processing: {e}")
             return
 
-        params = og.GeneratorParams(self.model)
-        params.set_inputs(inputs)
-        params.set_search_options(max_length=2048)
-        generator = og.Generator(self.model, params)
+        generation_args = {
+		"max_new_tokens": 2048,
+		"temperature": 0.0,
+		"do_sample": False,}
 
-        response = ""
+        generate_ids = self.model.generate(
+            **inputs,
+            eos_token_id=self.processor.tokenizer.eos_token_id,
+            **generation_args)
 
-        while not generator.is_done():
-            generator.compute_logits()
-            generator.generate_next_token()
-            new_token = generator.get_next_tokens()[0]
-            response += self.tokenizer_stream.decode(new_token)
+        # Remove input tokens
+        generate_ids = generate_ids[:, inputs['input_ids'].shape[1]:]
 
-        del generator
+        response = self.processor.batch_decode(
+            generate_ids,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False)[0]
 
         output.put(response)
         output.put(None)
@@ -196,9 +215,9 @@ def load(request: LoadRequest):
     if request.model_id not in models:
         for gpu_index in range(request.num_gpus):
             for _ in range(request.models_per_gpu):
-                model = Model(
-                    "..\\Phi-3-vision-128k-instruct-onnx-directml\\directml-int4-rtn-block-32",
-                    gpu_index)
+                model_path = "app/models/phi/directml-int4-rtn-block-32"
+                model = Model(model_path,
+                              gpu_index)
                 model.load()
                 models[request.model_id] = model
     return {"status": "200 OK", "model_id": request.model_id}
